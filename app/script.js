58892cf3321203cc695e221f6b1010b601eb747a
Aline Silva <aline@email.com>
2026-02-05 15:56:26 +0000

    including the alert button in the js file

--- app/script.js
+++ app/script.js
@@ -1,2 +1,7 @@
-console.log('Hello World!')
-console.log("my name is Dona Aline!")
+console.log("Backend branch loaded");
+
+const button = document.getElementById("button01");
+
+button.addEventListener("click", () => {
+    alert("Task completed! Notification sent.");
+});
